7023393091fa937af5ce2b4497ce83c27326b978
RABANI <sinsenju537@gmail.com>
2024-08-16 11:07:54 +0700

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import { HomePage } from "./components/Hero/home";
@@ -14,9 +15,6 @@ import { CoffeProducts } from "./components/coffee-product/coffe-products";
 import Blog from "./components/Blog/Blog";
 import Footer from "./components/Footer/footer";
 import { SpecialProduct } from "./components/SpecialProduct/Special-product";
-import FormPage from "./components/Form/Form";
-import CategoriPage from "./pages/Categori";
-import Cart from "./components/Cart/Cart";
 import { useUser } from "./utility/Store";
 import TopCategori from "./components/Top Categori/topCategori";
 import { Toaster } from "react-hot-toast";
@@ -33,16 +31,22 @@ function App() {
     setUser(Storege);
   }, []);
 
+  const FormPage = lazy(() => import("./components/Form/Form"));
+  const CategoriPage = lazy(() => import("./pages/Categori"));
+  const Cart = lazy(() => import("./components/Cart/Cart"));
+
   return (
     <Router>
       <div className="h-fit bg-[#F9F6F1]" id="main-container">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<MainContent />} />
-          <Route path="/auth" element={<FormPage />} />
-          <Route path="/Categori/:type" element={<CategoriPage />} />
-          <Route path="/Cart" element={<Cart />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<MainContent />} />
+            <Route path="/auth" element={<FormPage />} />
+            <Route path="/Categori/:type" element={<CategoriPage />} />
+            <Route path="/Cart" element={<Cart />} />
+          </Routes>
+        </Suspense>
         <Toaster />
       </div>
     </Router>
