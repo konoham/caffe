aa8264fbfa364df75afce2794039b42381b8cebb
RABANI <sinsenju537@gmail.com>
2024-08-17 08:07:55 +0700

    refactoring

--- src/components/Hero/home.jsx
+++ src/components/Hero/home.jsx
@@ -26,14 +26,17 @@ export const HomePage = () => {
               className="relative block h-full w-full bg-top object-cover"
               src={item.img}
             />
-            <div className="container absolute left-1/2 top-1/2 me-auto translate-x-[-50%] px-6 md:px-0">
+            <div className="container absolute left-1/2 top-1/2 mx-auto me-auto translate-x-[-50%] px-6 md:px-0">
               <h1
                 className="mb-4 w-[60%] text-wrap text-2xl font-bold capitalize tracking-wide md:w-1/3 md:text-5xl"
                 id="textHome"
               >
                 {item.description}
               </h1>
-              <a className="mt-4 rounded-lg bg-primary px-3 py-2 text-sm font-semibold text-white hover:bg-gray-900 disabled:opacity-50 md:px-4 md:py-2">
+              <a
+                href="#special-Coffee"
+                className="mt-4 rounded-lg bg-primary px-3 py-2 text-sm font-semibold text-white hover:bg-gray-900 disabled:opacity-50 md:px-4 md:py-2"
+              >
                 Beli Sekarang
               </a>
             </div>
